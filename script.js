a2ab93fa51c4804643dec746188447ae3cbc4a3e
Наврузов <167851240+mohmadnavruzov084@users.noreply.github.com>
2025-01-25 00:59:39 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+window.addEventListener('DOMContentLoaded', () => {
+  const menu = document.querySelector('.menu'),
+      menuItem = document.querySelectorAll('.menu_item'),
+      hamburger = document.querySelector('.hamburger');
+
+  hamburger.addEventListener('click', () => {
+      hamburger.classList.toggle('hamburger_active');
+      menu.classList.toggle('menu_active');
+  });
+
+  menuItem.forEach(item => {
+      item.addEventListener('click', () => {
+          hamburger.classList.toggle('hamburger_active');
+          menu.classList.toggle('menu_active');
+      })
+  })
+})
+
+
+var swiper = new Swiper('.swiper', {
+  slidesPerView: 1.3,
+  direction: getDirection(),
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  on: {
+    resize: function () {
+      swiper.changeDirection(getDirection());
+    },
+  },
+});
+
+function getDirection() {
+  var windowWidth = window.innerWidth;
+  var direction = window.innerWidth >= 1440 ? 'vertical' : 'horizontal';
+
+  return direction;
+}
